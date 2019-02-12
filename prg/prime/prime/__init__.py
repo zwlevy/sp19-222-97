10618dfcefdb4c0018268b7809c8c6ca1b76a449
zwlevy <46494607+zwlevy@users.noreply.github.com>
2019-02-12 14:49:01 -0500

    Add files via upload

---
+++ prg/prime/prime/__init__.py
@@ -0,0 +1,1 @@
+# We don't typically want to add the file for our main stuff into __init__
---
+++ prg/prime/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+from prime import prime
+
+setup(name = 'prime',
+      version = '1.0.1',
+      url = '',
+      author = '',
+      packages = [''])
